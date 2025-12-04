443d6f780f09dc98f3fe1ee7bb3f04d4448124c8
khadija2378 <khadijaraja119@gmail.com>
2025-12-04 11:58:59 +0100

    get events

--- Frontend/src/components/EventCard.jsx
+++ Frontend/src/components/EventCard.jsx
@@ -1,4 +1,11 @@
+import { useContext } from "react";
+import { AppContext } from "../contexts/AppContext";
+import { Calendar, MapPin } from "lucide-react"
+
+
 function EventCard(){
+    const {events}=useContext(AppContext);
+
     return(
         <section className="px-6 md:px-12 py-12">
 <div className="flex items-center justify-between mb-6">
@@ -11,16 +18,25 @@ function EventCard(){
 
 
 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-{[1, 2, 3, 4, 5, 6].map((i) => (
-<div key={i} className="bg-white rounded-xl shadow hover:shadow-lg transition p-3">
-<img src="/event.jpg" className="w-full h-48 object-cover rounded-lg" alt="event"/>
-<h3 className="font-semibold mt-3">Festival de Musique Électronique 2025</h3>
-<div className="text-sm text-gray-600 mt-2">
-<p>📅 15 Juillet 2025</p>
-<p>📍 Parc des Expositions, Paris</p>
-</div>
+{events.map((event) => (
+<div key={event.id} className="bg-white rounded-xl shadow hover:shadow-lg transition p-3">
+<img src={event.image_path} className="w-full h-48 object-cover rounded-lg" alt="event"/>
+<h3 className="font-semibold mt-3">{event.title}</h3>
+
+<div className="mt-4 space-y-2 text-gray-700">
+                <div className="flex items-center gap-2 text-sm">
+                  <Calendar className="w-5 h-5 text-gray-800" />
+                  <span>{event.date}</span>
+                </div>
+
+                <div className="flex items-center gap-2 text-sm">
+                  <MapPin className="w-5 h-5 text-gray-800" />
+                  <span>{event.location}</span>
+                </div>
+              </div>
+
 <div className="flex items-center justify-between mt-4">
-<span className="text-[#40916C] font-semibold">54 DH</span>
+<span className="text-[#40916C] font-semibold">{event.price} DH</span>
 <button className="px-4 py-1 border border-[#40916C] text-[#40916C] rounded-full hover:bg-[#40916C] hover:text-white transition text-sm">
 voir détails
 </button>
